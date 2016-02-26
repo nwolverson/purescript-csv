cb3f96b47e5467e979fc804f885602fe9def41b8
robkuz <robert@robkuz.com>
2016-02-26 17:17:48 +0100

    added config wrapper API

--- src/Text/Parsing/CSV.purs
+++ src/Text/Parsing/CSV.purs
@@ -1,6 +1,6 @@
 module Text.Parsing.CSV where
 
-import Prelude ((<$>), return, ($), bind, (/=), (&&))
+import Prelude ((<$>), return, ($), bind, (/=), (&&), map, id, (<*>), (++))
 import Text.Parsing.Parser (Parser)
 import Text.Parsing.Parser.Combinators (sepEndBy, sepBy1, between)
 import Text.Parsing.Parser.String (eof, string, satisfy)
@@ -9,43 +9,82 @@ import Control.Alt ((<|>))
 import Control.Apply ((<*))
 import Data.Functor (($>))
 
-import Data.Array(some)
-import Data.String(fromCharArray)
-import Data.List(List(..),zip)
+import Data.Array (some)
+import Data.String (fromCharArray, toCharArray)
+import Data.Char (toString)
+import Data.List (List(..),zip)
+import Data.Foldable (all)
 
 import Data.Map as M
 
+import Data.Maybe
+
 type P a = Parser String a
 
-quoted :: forall a. P a -> P a
-quoted p = between (string "\"") (string "\"") p
+type Parsers a =
+  {
+    quoted :: (P a -> P a),
+    chars :: P String,
+    qchars :: P String,
+    field :: P String,
+    row :: P (List String),
+    file :: P (List (List String)),
+    fileHeaded :: P (List (M.Map String String))
+  }  
 
-chars :: P String
-chars = fromCharArray <$> some char
+makeQuoted :: forall a. String -> (P a -> P a)
+makeQuoted q = between (string q) (string q)
+
+makeChars :: String -> P String
+makeChars xs = do
+  fromCharArray <$> some char
   where
-  char = satisfy (\c -> c /= '"' && c /= '\n' && c /= ',')
+    char = satisfy $ excluded xs
+    excluded xs = \x -> all id $ terms xs <*> [x]
+    terms xs = map (/=) $ toCharArray xs
 
-qchars :: P String
-qchars = fromCharArray <$> some (qchar <|> escapedQuote)
+makeQchars :: Char -> P String
+makeQchars c = fromCharArray <$> some (qchar <|> escapedQuote)
   where
   escapedQuote :: P Char
-  escapedQuote = string "\"\"" $> '"'
-  qchar = satisfy (\c -> c /= '"')
+  escapedQuote = (string $ (toString c ++ toString c)) $> c
+  qchar = satisfy (\c' -> c' /= c)
 
-field :: P String
-field = quoted qchars <|> chars
+makeField :: forall a. (P String -> P String) -> P String -> P String -> P String
+makeField q qc c = q qc <|> c
 
-row :: P (List String)
-row = field `sepBy1` string ","
+makeRow :: String -> P String -> P (List String)
+makeRow s f = f `sepBy1` string s
 
-file :: P (List (List String))
-file = row `sepEndBy` string "\n" <* eof
+makeFile :: String -> P (List String) -> P (List (List String))
+makeFile s r = r `sepEndBy` string s <* eof
 
-fileHeaded :: P (List (M.Map String String))
-fileHeaded = do
+makeFileHeaded :: P (List (List String)) -> P (List (M.Map String String))
+makeFileHeaded file = do
   f <- file
   return $ case f of
     Nil -> Nil
     Cons header rows -> mkRow header <$> rows
   where
     mkRow header row' = M.fromList $ zip header row'
+
+makeParsers :: forall a. Char -> String -> String -> Parsers String
+makeParsers quote seperator eol = do
+  let quoted' = makeQuoted $ toString quote
+  let chars' = makeChars $ (toString quote) ++ seperator ++ eol
+  let qchars' = makeQchars quote
+  let field' = makeField quoted' qchars' chars'
+  let row' = makeRow seperator field'
+  let file' = makeFile eol row'
+  let fileHeaded' = makeFileHeaded file'
+  {
+    quoted: quoted',
+    chars: chars',
+    qchars: qchars',
+    field: field',
+    row: row',
+    file: file',
+    fileHeaded: fileHeaded'
+  }  
+
+defaultParsers = makeParsers '"' "," "\n"
--- test/Main.purs
+++ test/Main.purs
@@ -7,7 +7,7 @@ import Control.Monad.Eff (Eff)
 import Test.Unit (TIMER, test, runTest)
 import Test.Unit.Assert (assert)
 import Test.Unit.Console (TESTOUTPUT ())
-import Text.Parsing.CSV (P, fileHeaded, file, row, field, chars)
+import Text.Parsing.CSV (P, defaultParsers, makeParsers)
 import Data.Either (Either(Left, Right))
 import Data.Maybe (fromMaybe)
 
@@ -15,6 +15,8 @@ import Text.Parsing.Parser (runParser)
 
 import Data.List(toList,List(),head)
 import Data.Map as M
+
+excelParser = makeParsers '\'' ";" "\r\n"
 
 parseTrue :: forall a. P a -> (a -> Boolean) -> String -> Boolean
 parseTrue parser expectation input =
@@ -24,12 +26,6 @@ parseTrue parser expectation input =
 
 parses :: forall a. (Eq a) => P a -> a -> String -> Boolean
 parses parser expected input = parseTrue parser (\r -> expected == r) input
-
-noparse :: forall a. (Eq a) => P a -> String -> Boolean
-noparse parser input =
-  case runParser input parser of
-    Right _ -> false
-    Left _ -> true
 
 testFile :: String
 testFile = """a,b,c
@@ -46,26 +42,31 @@ testFileResult = toList $ toList <$> [
 main :: forall a. Eff (testOutput :: TESTOUTPUT, avar :: AVAR, timer :: TIMER | a) Unit
 main = runTest do
   test "chars" do
-    assert "parses chars" $ parses chars "abc" "abc"
-    assert "doesn't parse with quote" $ parses chars "ab" "ab\"c"
+    assert "parses chars" $ parses defaultParsers.chars "abc" "abc"
+    assert "doesn't parse with quote" $ parses defaultParsers.chars "ab" "ab\"c"
   test "unquoted field" do
-    assert "" $ parses field "abc123" "abc123"
+    assert "" $ parses defaultParsers.field "abc123" "abc123"
   test "quoted field" do
-    assert "" $ parses field "abc123" "\"abc123\""
+    assert "quoted with \": " $ parses defaultParsers.field "abc123" "\"abc123\""
+    assert "cant parse ' quotation': " $ parses excelParser.field "abc123" "'abc123'"
+    assert "can have seperator in quoted field" $ parses excelParser.field "abc;123" "'abc;123'"
   test "quoted field with quotes" do
-    assert "doesn't allow quote escape" $ parses field "x\"y" "\"x\"\"y\""
+    assert "doesn't allow quote escape" $ parses defaultParsers.field "x\"y" "\"x\"\"y\""
   test "newlines" do
-    assert "can't have newline in unquoted field" $ parses field "a" "a\nb"
-    assert "can have newline in quoted field" $ parses field "a\nb" "\"a\nb\""
+    assert "can't have newline in unquoted field" $ parses defaultParsers.field "a" "a\nb"
+    assert "can have newline in quoted field" $ parses defaultParsers.field "a\nb" "\"a\nb\""
   test "row" do
-    assert "failed basic row" $ parses row (toList $ ["a", "b", "c"]) "a,b,c"
-    assert "failed quoted row" $ parses row (toList $ ["a", "b", "c"]) "\"a\",\"b\",\"c\""
+    assert "failed basic row" $ parses defaultParsers.row (toList $ ["a", "b", "c"]) "a,b,c"
+    assert "failed quoted row" $ parses defaultParsers.row (toList $ ["a", "b", "c"]) "\"a\",\"b\",\"c\""
+    assert "failed basic row" $ parses excelParser.row (toList $ ["a", "b", "c"]) "a;b;c"
+    assert "failed quoted row" $ parses excelParser.row (toList $ ["a", "b", "c"]) "'a';'b';'c'"
   test "file" do
-    assert "single line file" $ parses file testFileResult "a,b,c\n1,2,3\nx,y,z"
-    assert "didn't parse file" $ parses file testFileResult testFile
-    assert "didn't parse file with trailing newline" $ parses file testFileResult $ testFile ++ "\n"
+    assert "single line file" $ parses defaultParsers.file testFileResult "a,b,c\n1,2,3\nx,y,z"
+    assert "single line file with windows eols" $ parses excelParser.file testFileResult "a;b;c\r\n1;2;3\r\nx;y;z"
+    assert "didn't parse file" $ parses defaultParsers.file testFileResult testFile
+    assert "didn't parse file with trailing newline" $ parses defaultParsers.file testFileResult $ testFile ++ "\n"
   test "fileHeaded" do
-    assert "headed lookup" $ parseTrue fileHeaded (\res -> fromMaybe false $ do
+    assert "headed lookup" $ parseTrue defaultParsers.fileHeaded (\res -> fromMaybe false $ do
       row <- head res
       a <- M.lookup "a" (row :: M.Map String String)
       b <- M.lookup "b" row
